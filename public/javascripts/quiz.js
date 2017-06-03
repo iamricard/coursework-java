fc1b6d43b580c93c3e4361d7a3dc935b77e0c655
Ricard Solé <ricardsolecasas@gmail.com>
2017-06-03 23:24:49 +0100

    fix(quiz): removes _previous_ quiz showing

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -3,18 +3,17 @@ function boot() {
 
   app.addEventListener('click', evt => {
     const current = document.querySelector('.current')
-    const previous = document.querySelector('.previous')
     const form = document.querySelector('form')
 
     if (current.contains(evt.target) && evt.target.tagName === 'INPUT') {
       const next = current.nextElementSibling
-      current.classList.remove('current')
-      current.classList.add('previous')
 
-      if (previous) previous.classList.remove('previous')
-      if (!next) form.submit()
-
-      next.classList.add('current')
+      if (!next) {
+        form.submit()
+      } else {
+        current.classList.remove('current')
+        next.classList.add('current')
+      }
     }
   })
 }
